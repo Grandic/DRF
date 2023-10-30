4bbee8048ad3252e35ab8b5e4147b702636b48bd
Grandic <d.bitkov@gmail.com>
2023-10-30 22:44:19 +0300

    * was complete

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,14 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from payments.models import Payments
 from payments.serializers import PaymentSerializer
+from rest_framework.filters import OrderingFilter
 
 
 class PaymentsViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'payment_type_cash',
+                        'payment_type_credit_card')
+    ordering_fields = ('payment_date',)
--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
+from payments.models import Payments
 from users.models import User
 
 
 class Userserializer(serializers.ModelSerializer):
+    payment = SerializerMethodField()
+
+    def get_payment(self, user):
+        return [pay.payment_date for pay in Payments.objects.filter(user=user)]
+
+    # def get_payment(self, user):
+    #     return Payments.objects.filter(user=user)
 
     class Meta:
         model = User
         fields = '__all__'
-
