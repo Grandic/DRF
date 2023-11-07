f10a19da52c936942e2c01b039e4a0544f9b50d6
Grandic <d.bitkov@gmail.com>
2023-11-08 01:01:15 +0300

    perm

--- lesson/views.py
+++ lesson/views.py
@@ -9,30 +9,25 @@ from lesson.serializers import LessonSerializer, LessonListSerializer
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
-    permission_classes = [IsAuthenticated]
-    # permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
+    permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonListSerializer
     queryset = Lesson.objects.all()
     pagination_class = CoursePaginator
-    permission_classes = [IsAuthenticated]
-    # permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
+    permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
-    # permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
+    permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
-    # permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsAuthenticated]
-    # permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
+    permission_classes = [IsAuthenticated, IsSuperUser | IsModerator | IsOwner]
